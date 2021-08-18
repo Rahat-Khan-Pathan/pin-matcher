6ea5f9a18a308d80bc56ec62e391288f8f3b9d97
Rahat-Khan-Pathan <rahatkhanpathan99@gmail.com>
2021-08-18 06:05:11 +0600

    up

--- app.js
+++ app.js
@@ -5,26 +5,13 @@ const myPinViewer = document.getElementById('my-pin-viewer');
 const submitButton = document.getElementById('submit-btn')
 const notifyYes = document.getElementById('notify-yes');
 const notifyNo = document.getElementById('notify-no');
-const backspaceButton = document.getElementById('backspace-btn');
-const clearButton = document.getElementById('clear-btn');
 const totalTry = document.getElementById('try');
-
-// All buttons from 0-9
-const button0 = document.getElementById('btn-0');
-const button1 = document.getElementById('btn-1');
-const button2 = document.getElementById('btn-2');
-const button3 = document.getElementById('btn-3');
-const button4 = document.getElementById('btn-4');
-const button5 = document.getElementById('btn-5');
-const button6 = document.getElementById('btn-6');
-const button7 = document.getElementById('btn-7');
-const button8 = document.getElementById('btn-8');
-const button9 = document.getElementById('btn-9');
 
 
 var value;
 var myValue='';
 var count=3; //try
+
 function showPin(value)
 {
     pinViewer.value = value;
@@ -37,6 +24,8 @@ function showTry(count)
 {
     totalTry.innerHTML = count;
 }
+
+// Generate button 
 generateButton.addEventListener('click',function(event)
 {
     value=Math.ceil(Math.random()*9999);
@@ -47,67 +36,12 @@ generateButton.addEventListener('click',function(event)
     myValue='';
     showMyPin(myValue);
 })
-button0.addEventListener('click',function(event)
-{
-    myValue+='0';
-    showMyPin(myValue);
-})
-button1.addEventListener('click',function(event)
-{
-    myValue+='1';
-    showMyPin(myValue);
-})
-button2.addEventListener('click',function(event)
-{
-    myValue+='2';
-    showMyPin(myValue);
-})
-button3.addEventListener('click',function(event)
-{
-    myValue+='3';
-    showMyPin(myValue);
-})
-button4.addEventListener('click',function(event)
-{
-    myValue+='4';
-    showMyPin(myValue);
-})
-button5.addEventListener('click',function(event)
-{
-    myValue+='5';
-    showMyPin(myValue);
-})
-button6.addEventListener('click',function(event)
-{
-    myValue+='6';
-    showMyPin(myValue);
-})
-button7.addEventListener('click',function(event)
-{
-    myValue+='7';
-    showMyPin(myValue);
-})
-button8.addEventListener('click',function(event)
-{
-    myValue+='8';
-    showMyPin(myValue);
-})
-button9.addEventListener('click',function(event)
-{
-    myValue+='9';
-    showMyPin(myValue);
-})
-backspaceButton.addEventListener('click',function(event)
-{
-    myValue = myValue.slice(0,-1);
-    showMyPin(myValue);
-})
-clearButton.addEventListener('click',function(event)
-{
-    myValue='';
-    showMyPin(myValue);
-})
 
+// All buttons using eventBubble 
+document.getElementById('key-pad').addEventListener('click',function(event){
+    myValue+=(event.target.innerText);
+    showMyPin(myValue);
+})
 submitButton.addEventListener('click',function(event)
 {
     notifyYes.style.display = 'none';
